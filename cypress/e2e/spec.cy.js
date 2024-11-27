cf1780717b71bff687f14ab176607cc4589797de
Samuel <samuel.alves@coreplan.com.br>
2024-11-27 15:00:29 -0300

    teste de commit

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,10 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://example.cypress.io')
+    cy.contains('type').click()
+
+    // Should be on a new URL which
+    // includes '/commands/actions'
+    cy.url().should('include', '/commands/actions')
   })
 })
